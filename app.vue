2283f1a6b02ca6e9fb292c81ce9e99c173713f03
Abajit Dey <abajitdeyag10118@gmail.com>
2024-02-25 00:34:28 +0530

    First commit, added fonts, Navigation component
    Added fonts, tailwind, navigation component

---
+++ app.vue
@@ -0,0 +1,31 @@
+<template>
+  <div class="md:px-8">
+    <Navigation />
+  </div>
+</template>
+
+<script setup>
+import "@fontsource-variable/lora";
+import "@fontsource-variable/inter-tight";
+</script>
+
+<style>
+h1,
+h2,
+h3,
+h4 {
+  font-family: "Lora Variable", serif;
+}
+
+p,
+span,
+button,
+li {
+  font-family: "Inter Tight Variable", sans-serif;
+}
+
+button {
+  font-weight: 500;
+  font-size: 0.9rem;
+}
+</style>
---
+++ components/Navigation.vue
@@ -0,0 +1,30 @@
+<template>
+  <nav class="py-4 mt-4 flex flex-row items-center justify-between">
+    <div class="flex flex-row items-center">
+      <div class="border-r-2 border-slate-700 pr-8 mr-8">
+        <NuxtImg src="/logo.png" width="80" />
+      </div>
+      <ul class="flex flex-row items-center gap-8">
+        <li>Stories</li>
+        <li>Creator</li>
+        <li>Community</li>
+        <li>Subscribe</li>
+      </ul>
+    </div>
+    <div class="flex flex-row gap-4 items-center">
+      <Bell :size="18" />
+      <button>Sign In</button>
+    </div>
+  </nav>
+</template>
+
+<script setup>
+import { Bell } from "lucide-vue-next";
+</script>
+
+<style>
+li {
+  font-weight: 500;
+  font-size: 0.9rem;
+}
+</style>
